29b2abc54cc1f781b89b0cf29b681bf2b68af1e1
Stavitskii <pochta100@mail.ru>
2022-09-27 07:06:48 +0300

    00:47:36

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,38 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView
+
+from ads.models import Category
 
 
 def root(request):
     return JsonResponse({'status': 'ok'})
+
+
+@method_decorator(csrf_extemp, name='dispatch')
+class CategoryView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        result = []
+        for cat in categories:
+            result.append({"id": cat.id, "name": cat.name})
+        return JsonResponse(result, safe=False, json_dumps_params={'ensure_ascii': False})
+
+    def post(self, request):
+        data = json.loads(request.body)
+        new_category = Category.objects.create(name=data['name'])
+        return JsonResponse({'id': new_category.id, 'name': new_category.name}, safe=False,
+                            json_dumps_params={'ensure_ascii': False})
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+    def get(self, request, *args, **kwargs):
+        cat = self.get_object()
+        return JsonResponse({'id': cat.id, 'name': cat.name}, safe=False,
+                            json_dumps_params={'ensure_ascii': False})
+
+

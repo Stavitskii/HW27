a9a5e784a485f0217b7f6ffbc444306554290739
Stavitskii <pochta100@mail.ru>
2022-09-27 03:15:53 +0300

    00:25:06

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Ad(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    description = models.TextField(null=True)
+    address = models.CharField(max_length=100)
+    is_published = models.BooleanField(default=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)

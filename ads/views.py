409125c98613fd7d82e2adda9439c77283bf2cf9
Stavitskii <pochta100@mail.ru>
2022-09-27 07:48:26 +0300

    01:05:59

--- ads/views.py
+++ ads/views.py
@@ -7,7 +7,7 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView
 
-from ads.models import Category
+from ads.models import Category, Ad
 
 
 def root(request):
@@ -36,4 +36,55 @@ class CategoryDetailView(DetailView):
     def get(self, request, *args, **kwargs):
         cat = self.get_object()
         return JsonResponse({'id': cat.id, 'name': cat.name}, safe=False,
+                            json_dumps_params={'ensure_ascii': False})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AdView(View):
+    def get(self, request):
+        all_ads = Ad.objects.all()
+        result = []
+        for ad in all_ads:
+            result.append(
+                {"id": ad.id,
+                 "name": ad.name,
+                 "author": ad.author,
+                 "price": ad.price,
+                 "description": ad.description,
+                 "address": ad.address,
+                 "is_published": ad.is_published})
+        return JsonResponse(result, safe=False, json_dumps_params={'ensure_ascii': False})
+
+    def post(self, request):
+        data = json.loads(request.body)
+        new_ad = Ad.objects.create(
+            name=data['name'],
+            author=data['author'],
+            price=data['price'],
+            description=data['description'],
+            address=data['address'],
+            is_published=data['is_published']
+        )
+        return JsonResponse({'id': new_ad.id,
+                             'name': new_ad.name,
+                             'author': new_ad.author,
+                             'price': new_ad.price,
+                             'description': new_ad.description,
+                             'address': new_ad.address,
+                             'is_published': new_ad.is_published}, safe=False,
+                            json_dumps_params={'ensure_ascii': False})
+
+
+class AdDetailView(DetailView):
+    model = Ad
+
+    def get(self, request, *args, **kwargs):
+        ad = self.get_object()
+        return JsonResponse({"id": ad.id,
+                             "name": ad.name,
+                             "author": ad.author,
+                             "price": ad.price,
+                             "description": ad.description,
+                             "address": ad.address,
+                             "is_published": ad.is_published}, safe=False,
                             json_dumps_params={'ensure_ascii': False})
--- django_project/urls.py
+++ django_project/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', root),
     path('cat/', CategoryView.as_view()),
-    path('cat/<int:pk>', CategoryDetailView.as_view())
+    path('cat/<int:pk>', CategoryDetailView.as_view()),
+    path('ad/', AdView.as_view()),
+    path('ad/<int:pk>', AdDetailView.as_view())
 ]

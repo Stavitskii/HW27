94be0798a0821f944a23add42c0e0994b0f5f446
Stavitskii <pochta100@mail.ru>
2022-09-27 07:56:35 +0300

    finish commit

--- ads/models.py
+++ ads/models.py
@@ -9,6 +9,12 @@ class Ad(models.Model):
     address = models.CharField(max_length=100)
     is_published = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
